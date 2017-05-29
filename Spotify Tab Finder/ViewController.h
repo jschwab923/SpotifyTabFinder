b7260b2cb6923cc6ddf003d31afb893aefa78436
Jeff Schwab <jeff@feltapp.com>
2017-05-29 16:46:43 -0700

    Initial Commit

---
+++ Spotify Tab Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify Tab Finder
+//
+//  Created by Jeff Schwab on 5/29/17.
+//  Copyright © 2017 Jeff Writes Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Spotify Tab Finder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spotify Tab Finder
+//
+//  Created by Jeff Schwab on 5/29/17.
+//  Copyright © 2017 Jeff Writes Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
